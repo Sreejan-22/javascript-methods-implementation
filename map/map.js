a6e5684d7619f9016f712d82128e4aa1444c688b
Sreejan Chaudhury <sreejan147@gmail.com>
2021-06-23 20:49:44 +0530

    Update map.js

--- map/map.js
+++ map/map.js
@@ -1,5 +1,27 @@
 // Going by the definition given in MDN docs, "map() method creates a new array populated with the results of calling a provided function on every element in the calling array"
 
-const arr = [1, 2, 3, 4, 5];
-const newArr = arr.map((item) => item * 2)
-console.log(newArr); // [2, 4, 6, 8, 10]
+const sample = [1, 2, 3, 4];
+const mapResult = sample.map((item, index, array) => {
+  console.log(`item: ${item}, index: ${index}, array: ${array}`);
+  return item * 2;
+});
+console.log(mapResult);
+
+Array.prototype.mymap = function(callback) {
+  const resultArray = [];
+  for(let index=0;index<this.length;index++) {
+    resultArray.push(callback(this[index], index, this));
+  }
+  return resultArray;
+}
+
+console.log();
+
+const mymapRes = sample.mymap((item, index, array) => {
+  console.log(item + " " + index + " " + array);
+  return item * 3
+})
+
+console.log(mymapRes);
+
+
